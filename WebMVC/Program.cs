6454ae0c858e7f0ad41eee00b5e6154148d9d69f
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2024-01-17 19:45:21 +0700

    Update 17/1

--- WebMVC/Program.cs
+++ WebMVC/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    options.LoginPath = "/Login";
+    options.ReturnUrlParameter = "returnUrl";
+}).AddCookie("Admin", options =>
+{
+    options.LoginPath = new PathString("/Login");
+});
 
 var app = builder.Build();
 
@@ -18,6 +28,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
